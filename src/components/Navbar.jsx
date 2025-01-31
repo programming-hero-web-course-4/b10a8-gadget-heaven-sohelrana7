14a5f5bbd96f3ab7dcd209e67ff368f7218b47ac
sohelrana7 <sohel.rana.web1@gmail.com>
2025-01-31 17:29:34 +0600

    website name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Gadget Heaven</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 gap-6">{navLinks}</ul>
